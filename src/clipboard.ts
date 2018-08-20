ad4634950ffb24dedb8bc3424c6b375af3506b76
lym152898 <linyimin520812@gmail.com>
2018-08-20 12:54:01 +0800

    Add logger

--- src/clipboard.ts
+++ src/clipboard.ts
@@ -1,27 +1,30 @@
 import popUpNotify from './notification'
 import { spawnSync, exec } from 'child_process'
+import { logger } from './logger'
 interface Command {
   command: string,
   args: string[],
 }
 
-const pasteText: Command = {
-  command: 'xclip',
-  args: ["-selection", "clipboard", '-o'],
-}
+// const pasteText: Command = {
+//   command: 'xclip',
+//   args: ["-selection", "clipboard", '-o'],
+// }
 
 const pasteImage: Command = {
   command: 'xclip',
   args: ['-selection', 'clipboard', '-t', 'image/png', '-o'],
 }
-
+const log = logger()
 export function getContentFromCliboard(): Buffer {
   const result = spawnSync(pasteImage.command, pasteImage.args)
   const stdout = result.stdout
   if (result.status === 0 && stdout.length > 0) {
+    log.info('Get Image from clipboard success!')
     return Buffer.from(result.stdout)
   }
   popUpNotify()
+  log.error('Get Image from clipboard error!')
   throw new Error('The type of clipboard\' content is not image')
 }
 
--- src/upload.ts
+++ src/upload.ts
@@ -2,12 +2,13 @@
 import OSS from 'ali-oss'
 import cuid from 'cuid'
 import dotenv from 'dotenv'
+import { logger } from './logger'
 import popUpNotify from './notification'
 import { default as Keypress, TwoKeys, ThreeKeys } from 'keypress-event'
 import { getContentFromCliboard, copyContentToClipboard } from './clipboard'
 
 dotenv.config()
-
+const log = logger()
 
 let client = new OSS({
   region: process.env.region,
@@ -22,30 +23,36 @@ async function put(data: Buffer): Promise<string> {
   try {
     const imageName = `${cuid()}.png`
     let result = await client.put(imageName, data)
+    log.info('Upload image to OSS success')
     // copy the url to clipboard
     copyContentToClipboard(result.url)
+    log.info('Copy Content: %s To Clipboard', result.url)
     popUpNotify(data)
     // Change object acl to 'public-read', so we can view the picture by link
     client.putACL(imageName, 'public-read')
-      .then((result: any) => [
-      ])
+      .then((result: any) => {
+        log.info('Change image ACL to public-read success')
+      })
       .catch((err: Error) => {
-        console.log(err)
+        log.error('Change image ACL to pulic-read failed, The reason is %s', JSON.stringify(err, null, 2))
       })
     return result.url
   } catch (err) {
     popUpNotify()
+    log.error('Upload image to OSS failed, the reason is %s', JSON.stringify(err, null, 2))
     throw err
   }
 }
 
 function start(shortcut: TwoKeys | ThreeKeys) {
+  log.info('Register Shortcut: %s', shortcut.join('+'))
   Keypress.registerShortcut(shortcut, () => {
     try {
       const buffer = getContentFromCliboard()
       put(buffer)
+      log.info('Shortcut trigger and get content form clipboard success')
     } catch(err) {
-      console.log(err)
+      log.error('Shortcut trigger and get content form clipboard failed, the reason is %s', JSON.stringify(err, null, 2))
     }
   })
   Keypress.start()
